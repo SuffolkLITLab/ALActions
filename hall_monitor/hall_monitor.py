cf43edf98d519adfd76cf2c75424987edd74cc8d
Bryce Willey <bryce.willey@suffolk.edu>
2025-08-26 17:55:49 -0400

    Allow Hall monitor to just check the homepage
    We've started monitoring other org's servers now that we're hosting,
    so we need to ensure the DA server is functioning okay, but we don't need
    to individually check each interview live on the server.

--- hall_monitor/hall_monitor.py
+++ hall_monitor/hall_monitor.py
@@ -5,23 +5,35 @@ import bs4
 import requests
 import os
 
+def check_homepage(server_url):
+    with requests.get(f"{server_url}/") as conn:
+        if not conn.ok:
+            print(f"Hall monitor couldn't connect to {server_url}: {conn.status_code}")
+            return [f"{server_url}/"]
+    return []
+
 def check_server(server_url):
     with requests.get(f"{server_url}/list") as conn:
         if conn.ok:
             soup = bs4.BeautifulSoup(conn.text, "html.parser")
         else:
+            # TODO: return server_url/list as a failed_links?
             print(f"Hall monitor couldn't connect to {server_url}: {conn.status_code}")
             exit(1)
     links = soup.find_all("a")
     # These links are already checked by docassemble when the page is served
     failed_links = [link for link in links if "dainterviewhaserror" in (link.get("class") or [])]
-    return failed_links
+    return [fl.attrs['href'] for fl in failed_links]
 
 def main():
     server_url = os.environ['SERVER_URL']
-    failed_links = check_server(server_url)
+    if os.getenv('CHECK_TYPE') == 'homepage':
+        failed_links = check_homepage(server_url)
+    else:
+        failed_links = check_server(server_url)
+
     if failed_links:
-        updated_links = [f"{server_url}{fl.attrs['href']}" for fl in failed_links ]
+        updated_links = [f"{server_url}{fl}" for fl in failed_links ]
         err_str = f"Hall Monitor found these links that aren't installed correctly: {', '.join(updated_links)}"
         env_file = os.getenv('GITHUB_ENV')
         if env_file:
